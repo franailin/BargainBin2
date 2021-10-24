3f0945b54594c771815a9858a3e9e53e9b1d0fc3
Frank <fklin@berkeley.edu>
2021-10-24 00:09:12 -0700

    basic express app with a homepage, nothing else

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("May Node be with you")
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'templates/home.html')
+})
+
+var server = app.listen(8081, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+console.log('Server has started');
