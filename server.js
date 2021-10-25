aae5e51e8a2622d891defbcafbd990ae2fa3449b
Frank <fklin@berkeley.edu>
2021-10-24 21:09:44 -0700

    need file implementation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 const url = 'mongodb://127.0.0.1:27017';
+db_name = "BargainBin"
 
 app.get('/', function(req, res) {
     fs.readFile("G:/Frank/Website Project/templates/record.html", 'utf8', function(err, data){
