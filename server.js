ef363fcc43e00761607a7e245a677997ddbe5517
Frank <fklin@berkeley.edu>
2021-11-02 17:39:56 -0700

    moved some things over to server.js

--- server.js
+++ server.js
@@ -6,13 +6,43 @@ const ObjectID = require('mongodb').ObjectID;
 const url = 'mongodb://127.0.0.1:27017';
 db_name = "BargainBin"
 
-app.get('/', function(req, res) {
-    fs.readFile("G:/Frank/Website Project/templates/record.html", 'utf8', function(err, data){
-        
+app.get('/', (req, res) => {
+    fs.readFile('templates/home.html', (err, data) => {
+      if (err) {
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        return res.end("404 Not Found");
+      }
+      
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data); // Once we start populating our database with fake data, we can start replacing the "temps" in home.html
+      return res.end()
     })
 })
 
-var server = app.listen(8081, function(){
+app.post('/login', (req, res) => {
+    //let name = req.body.name;
+    //let password = req.body.password;
+    //console.log(`name: ${name}`);
+    //console.log(`password: ${password}`);
+    console.log(`body: ${req.body}`);
+    return res.end('received login info');
+})
+  
+app.post('/create_account', (req, res) => {
+    // get json string from req
+    let body = '';
+    req.setEncoding('utf8'); // Get the data as utf8 strings
+    req.on('error', err => {
+        body += chunk;
+    });
+    req.on('end', () => {
+      let newAuthor = JSON.parse(body);
+      // mongodb handles json here
+      addAuthor1(newAuthor);
+    });
+})
+
+var server = app.listen(8080, function(){
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
